727f2ae7be0924f1d23111c9e5e70d34675eb6a7
Anton Yakovchuk <yakovchuk.a.p@gmail.com>
2019-03-03 12:36:21 +0200

    add health check

---
+++ src/main/java/rest/HealthController.java
@@ -0,0 +1,15 @@
+package rest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HealthController {
+
+    @GetMapping("/health")
+    public ResponseEntity health() {
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+}
